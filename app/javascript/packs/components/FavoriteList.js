e1ae57ff85eafb0c979c851403a60ce960764fd4
Chisato Murakami <kiwipom1031@gmail.com>
2020-08-28 21:05:32 -0700

    add favorites route in navbar and display the list of user favorites in FavoriteList component

--- app/javascript/packs/components/FavoriteList.js
+++ app/javascript/packs/components/FavoriteList.js
@@ -1,21 +1,62 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import * as a from "../actions";
+import { connect } from "react-redux";
+import { Container } from "react-bootstrap";
 
 function FavoriteList(props) {
   const [flowers, setFlowers] = useState([]);
+  const { dispatch } = props;
 
+  useEffect(() => {
+    fetch("/api/favorites")
+      .then((response) => response.json())
+      .then((jsonifiedResponse) => {
+        console.log(jsonifiedResponse);
+        setFlowers(jsonifiedResponse)
+        // const action = a.loadedFavFlowers(jsonifiedResponse);
+        // dispatch(action);
+      });
+    return () => {};
+  }, []);
+
+  const showFavorites = () => {
+    return (
+      <React.Fragment>
+        {flowers &&
+          flowers.map((flower) => {
+            <div key={flower.id}>
+              <h1>{flower.title}</h1>
+            </div>;
+          })}
+      </React.Fragment>
+    );
+  };
   return (
     <React.Fragment>
-      <h4>Your favorites</h4>
-
       
+      {flowers && flowers.map((flower) => {
+        return(
+          <div key={flower.id}>{flower.title}</div>
+        )
+      })}
+
+     
+      {/* {props.flowers && props.flowers.map((flower) => {
+       <div key={flower.id}>
+         <h1>{flower.title}</h1>
+       </div>
+     })} */}
     </React.Fragment>
-  )
+  );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    flowers: state.favFlowerListReducer.flowers
-  }
-}
-Favoritelist = connect(mapStateToProps)(FavoriteList);
+//  const mapStateToProps = (state) => {
+//    console.log(state.favFlowerListReducer.flowers);
+//   return {
+//     flowers: state.favFlowerListReducer.flowers,
+//   };
+// };
+
+// FavoriteList = connect(mapStateToProps)(FavoriteList);
+
 export default FavoriteList;
--- app/javascript/packs/components/NavBar.js
+++ app/javascript/packs/components/NavBar.js
@@ -1,6 +1,14 @@
 import React from "react";
-import { Nav, Navbar, NavDropdown, Form, FormControl, Button, Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import {
+  Nav,
+  Navbar,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+  Card,
+} from "react-bootstrap";
+import { Link, LinkContainer } from "react-router-dom";
 import gurumiIcon from "./images/gurumi_icon.png";
 import gurumiSignInIcon from "./images/gurumi_signin_icon.png";
 import gurumiSignOutIcon from "./images/gurumi_signout_icon.png";
@@ -8,9 +16,8 @@ import { connect } from "react-redux";
 import * as a from "../actions";
 
 function NavBar(props) {
-
   const { dispatch } = props;
-  
+
   const signOut = () => {
     fetch("/users/sign_out", {
       method: "DELETE",
@@ -22,16 +29,17 @@ function NavBar(props) {
 
   const searchHandler = (event) => {
     event.preventDefault();
- 
-    fetch(`/api/flowers?search=${event.target.search.value}`)
-    .then((response) => response.json())
-    .then((jsonifiedResponse) => {
-      const action = a.loadedFlowers(jsonifiedResponse);
-      dispatch(action);
-    })
-  }
 
-   {/* <Nav.Item className='ml-3'>
+    fetch(`/api/flowers?search=${event.target.search.value}`)
+      .then((response) => response.json())
+      .then((jsonifiedResponse) => {
+        const action = a.loadedFlowers(jsonifiedResponse);
+        dispatch(action);
+      });
+  };
+
+  {
+    /* <Nav.Item className='ml-3'>
             <img height={25} className="mr-2" src={gurumiSignOutIcon} alt="gutumiSignoutIcon" />
             {props.currentUser.email}
           </Nav.Item>
@@ -39,26 +47,30 @@ function NavBar(props) {
             <Nav.Link className="link" onClick={signOut}>
               Sign Out
             </Nav.Link>
-          </Nav.Item> */}
+          </Nav.Item> */
+  }
   const userLoginStatus = () => {
     if (props.currentUser && props.currentUser.email) {
       return (
-           <NavDropdown title= {props.currentUser.email} id="basic-nav-dropdown">
-              <NavDropdown.Item onClick={signOut}>
-                Sign Out 
-            </NavDropdown.Item>
-              <NavDropdown.Item>
-                <Link as={Link} to="/favorites">
-                Your Favorites
-                </Link>
-            </NavDropdown.Item>
-          </NavDropdown>
+        <NavDropdown title={props.currentUser.email} id="basic-nav-dropdown">
+          <NavDropdown.Item onClick={signOut}>
+            Sign Out
+          </NavDropdown.Item>
+          <NavDropdown.Item className="link" as={Link} to="/favorites" > 
+            Your Favorites
+          </NavDropdown.Item>
+        </NavDropdown>
       );
     } else {
       return (
         <Nav.Item>
           <Nav.Link className="link" as={Link} to="/users/log_in">
-            <img className="mr-2" height={25} src={gurumiSignInIcon} alt="gutumiAccIcon" />
+            <img
+              className="mr-2"
+              height={25}
+              src={gurumiSignInIcon}
+              alt="gutumiAccIcon"
+            />
             {props.currentUser && props.currentUser.email}
             Sign In
           </Nav.Link>
@@ -85,22 +97,24 @@ function NavBar(props) {
           </Nav.Item>
         </Nav>
 
-          <Nav.Item>
-            <Form inline className="justify-content-between" onSubmit={searchHandler}>
-              <FormControl
-                type="text"
-                name="search"
-                placeholder="Search Flowers"
-                className=" mr-sm-2"
-              />
-              <Button className="btn" variant="outline-secondary" type="submit">
-                Search
-              </Button>
-            </Form>
-          </Nav.Item>
-          <Nav>
-            {userLoginStatus()}
-          </Nav>
+        <Nav.Item>
+          <Form
+            inline
+            className="justify-content-between"
+            onSubmit={searchHandler}
+          >
+            <FormControl
+              type="text"
+              name="search"
+              placeholder="Search Flowers"
+              className=" mr-sm-2"
+            />
+            <Button className="btn" variant="outline-secondary" type="submit">
+              Search
+            </Button>
+          </Form>
+        </Nav.Item>
+        <Nav>{userLoginStatus()}</Nav>
       </Navbar>
     </>
   );
