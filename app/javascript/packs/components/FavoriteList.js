f5a130d6fe6ad1ce25e5aeed99e91d25461e74d7
Chisato Murakami <kiwipom1031@gmail.com>
2020-09-01 11:15:32 -0700

    reload the page after clicking remove in favorite page

--- app/javascript/packs/components/FavoriteList.js
+++ app/javascript/packs/components/FavoriteList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Card, Container, Row, Col, Button} from "react-bootstrap";
+import { Card, Container, Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function FavoriteList() {
@@ -21,32 +21,37 @@ function FavoriteList() {
     })
       .then((response) => response.json())
       .then((jsonifiedResponse) => {
-        console.log(jsonifiedResponse)
-      })
-  }
+        console.log(jsonifiedResponse);
+      });
+    window.location.reload(false);
+  };
 
-
-  
   return (
     <React.Fragment>
       <Container>
         <div>
-            <h4 className="mt-4 mb-4">Your Favorites</h4>
+          <h4 className="mt-4 mb-4">Your Favorites</h4>
         </div>
         <Row>
           {flowers &&
             flowers.map((flower) => {
               return (
-                <Col lg={4} md={4} sm={6} key={flower.id} style={{ textAlign: "center" }}>
-                  <Link to={`/flower/${flower.id}`}>
-                    <Card
-                      border="light"
-                      style={{
-                        width: "100%",
-                        height: "500px",
-                        marginBottom: "30px",
-                      }}
-                    >
+                <Col
+                  lg={4}
+                  md={4}
+                  sm={6}
+                  key={flower.id}
+                  style={{ textAlign: "center" }}
+                >
+                  <Card
+                    border="light"
+                    style={{
+                      width: "100%",
+                      height: "500px",
+                      marginBottom: "30px",
+                    }}
+                  >
+                    <Link to={`/flower/${flower.id}`}>
                       <Card.Img
                         variant="top"
                         style={{
@@ -58,26 +63,22 @@ function FavoriteList() {
                           flower.flower_photos[0] && flower.flower_photos[0].url
                         }
                       />
-
-                      <Card.Body>
-                        <Card.Title>{flower.title}</Card.Title>
-                        <Card.Text>${flower.price}</Card.Text>
-                        <Button 
-                          variant="outline-secondary"
-                          className="mb-1 mr-1"
-                          onClick={() => removeHandler(flower.id)}
-                        >
-                          Remove
-                        </Button>
-                        <Button 
-                          variant="outline-secondary"
-                          className="mb-1"
-                        >
-                          Move to Cart 
-                        </Button>
-                      </Card.Body>
-                    </Card>
-                  </Link>
+                    </Link>
+                    <Card.Body>
+                      <Card.Title>{flower.title}</Card.Title>
+                      <Card.Text>${flower.price}</Card.Text>
+                      <Button
+                        variant="outline-secondary"
+                        className="mb-1 mr-1"
+                        onClick={() => removeHandler(flower.id)}
+                      >
+                        Remove
+                      </Button>
+                      <Button variant="outline-secondary" className="mb-1">
+                        Move to Cart
+                      </Button>
+                    </Card.Body>
+                  </Card>
                 </Col>
               );
             })}
