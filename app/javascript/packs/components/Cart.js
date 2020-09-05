853ebf57f7011ee6aac08a3dc643993f13cbf281
Chisato Murakami <kiwipom1031@gmail.com>
2020-09-05 13:04:58 -0700

    add Remove button and select box in Cart

--- app/javascript/packs/components/Cart.js
+++ app/javascript/packs/components/Cart.js
@@ -1,28 +1,29 @@
 import React, { useEffect, useState } from "react";
 import FavoriteList from "./FavoriteList";
-import { Container, Image, Row, Col } from "react-bootstrap";
+import { Container, Image, Row, Col, Button, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Cart() {
   const [flowers, setFlowers] = useState([]);
 
   useEffect(() => {
     fetch("/api/cart")
-    .then((response) => response.json())
-    .then((jsonifiedResponse) => {
-      setFlowers(jsonifiedResponse);
-      console.log(jsonifiedResponse)
-    });
+      .then((response) => response.json())
+      .then((jsonifiedResponse) => {
+        setFlowers(jsonifiedResponse);
+        console.log(jsonifiedResponse);
+      });
     return () => {};
   }, []);
 
   const removeHandler = (id) => {
     fetch(`/cart_delete/${id}`)
-    .then((response) => response.json())
-    .then((jsonifiedResponse) => {
-      console.log(jsonifiedResponse)
-    });
+      .then((response) => response.json())
+      .then((jsonifiedResponse) => {
+        console.log(jsonifiedResponse);
+      });
     window.location.reload(false);
-  }
+  };
   return (
     <React.Fragment>
       <Container>
@@ -30,32 +31,59 @@ function Cart() {
           <h4 className="mt-4 mb-4">Your Cart</h4>
         </div>
 
-          {flowers && flowers.map((flower) => {
+        {flowers &&
+          flowers.map((flower) => {
             return (
-              <Row key={flower.id}>  
-                  <Col lg={3}>
-                      <Image 
-                        src={flower.flower_photos && flower.flower_photos[0].url}
-                        style={{
-                          width: "150px",
-                          height: "150px",
-                        }}
-                      />
-                  </Col>
-                  <Col lg={6}>
-                        <p key={flower.id}>{flower.title}</p>
-                  </Col>
-                  <Col lg={3}>
-                    <p>{flower.price}</p>
-                  </Col>     
+              <Row key={flower.id} style={{ marginBottom: "15px" }}>
+                <Col lg={3}>
+                  <Link to={`/flower/${flower.id}`}>
+                    <Image
+                      src={flower.flower_photos && flower.flower_photos[0].url}
+                      style={{
+                        width: "150px",
+                        height: "150px",
+                      }}
+                    />
+                  </Link>
+                </Col>
+                <Col lg={6}>
+                  <Row>
+                    <h5 key={flower.id}>{flower.title}</h5>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <Button
+                        className="btn"
+                        variant="outline-secondary"
+                        className="mb-3"
+                      >
+                        Remove
+                      </Button>
+                    </Col>
+                    <Col>
+                      <Form>
+                        <Form.Group controlId="exampleForm.SelectCustom">
+                          <Form.Control as="select" custom>
+                            <option>Qty 1</option>
+                            <option>Qty 2</option>
+                            <option>Qty 3</option>
+                          </Form.Control>
+                        </Form.Group>
+                      </Form>
+                    </Col>
+                  </Row>
+                </Col>
+                <Col lg={3}>
+                  <h5>${flower.price}</h5>
+                </Col>
               </Row>
-            )
+            );
           })}
 
         <FavoriteList />
       </Container>
     </React.Fragment>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
