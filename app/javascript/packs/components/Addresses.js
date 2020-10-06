b04195ac3f90c3eb1f89026467f12c1b3de40b3e
Chisato Murakami <kiwipom1031@gmail.com>
2020-10-06 14:17:55 -0700

    add default column for addresses table, change the order to display addresses by setting as default

--- app/javascript/packs/components/Addresses.js
+++ app/javascript/packs/components/Addresses.js
@@ -43,57 +43,75 @@ function Addresses(props) {
       });
   };
 
+  const ifDefault = (boolean) => {
+    if (boolean) {
+      return (
+        <React.Fragment>
+          <p>Default</p>
+          <hr />
+        </React.Fragment>
+      );
+    }
+  };
+
   return (
     <React.Fragment>
       <Container>
         <h4>Your Addresses</h4>
         <Row>{showMessage()}</Row>
         <Row className="mt-3">
-          <Col xs={12} sm={12} md={4} lg={4}>
+          <Col xs={12} sm={12} md={3} lg={3}>
             <Link to={"/newaddress"}>
               <div className="address-add-btn">
                 <p>Add Address</p>
               </div>
             </Link>
           </Col>
-          {addresses.map((address) => {
-            return (
-              <Col xs={12} sm={12} md={4} lg={4} key={address.id}>
-                <div className="address">
-                  <p>
-                    {address.first_name} {address.last_name}
-                  </p>
-                  <p>
-                    <span>{address.street} </span>
-                    {address.apt_ste_unit && (
-                      <span>#{address.apt_ste_unit}</span>
-                    )}
-                  </p>
-                  <p>
-                    {address.city}, {address.state} {address.zip}
-                  </p>
-                  <p>Phone: {address.phone}</p>
-                  <p>
-                    <span>
-                      <Link to={`/editaddress/${address.id}`}>
-                        <Button
-                          variant="outline-secondary"
-                          className="mb-1 mr-1"
-                        >
-                          Edit
+          {addresses
+            .sort((a, b) => (a.default < b.default ? 1 : -1))
+            .map((address) => {
+              return (
+                <Col xs={12} sm={12} md={4} lg={3} key={address.id}>
+                  <div className="address">
+                    {ifDefault(address.default)}
+                    <p>
+                      {address.first_name} {address.last_name}
+                    </p>
+                    <p>
+                      <span>{address.street} </span>
+                      {address.apt_ste_unit && (
+                        <span>#{address.apt_ste_unit}</span>
+                      )}
+                    </p>
+                    <p>
+                      {address.city}, {address.state} {address.zip}
+                    </p>
+                    <p>Phone: {address.phone}</p>
+                    <p>
+                      <span>
+                        <Link to={`/editaddress/${address.id}`}>
+                          <Button variant="outline-secondary" className="mb-1">
+                            Edit
+                          </Button>
+                        </Link>
+                      </span>{" "}
+                      <span onClick={() => deleteHandler(address.id)}>
+                        <Button variant="outline-secondary" className="mb-1">
+                          Remove
                         </Button>
-                      </Link>
-                    </span>{" "}
-                    <span onClick={() => deleteHandler(address.id)}>
-                      <Button variant="outline-secondary" className="mb-1 mr-1">
-                        Remove
-                      </Button>
-                    </span>
-                  </p>
-                </div>
-              </Col>
-            );
-          })}
+                      </span>
+                      {!address.default && (
+                        <span>
+                          <Button variant="outline-secondary" className="mb-1">
+                            Set as default
+                          </Button>
+                        </span>
+                      )}
+                    </p>
+                  </div>
+                </Col>
+              );
+            })}
         </Row>
       </Container>
     </React.Fragment>
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_01_233710) do
+ActiveRecord::Schema.define(version: 2020_10_06_191501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_10_01_233710) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.boolean "default", default: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
