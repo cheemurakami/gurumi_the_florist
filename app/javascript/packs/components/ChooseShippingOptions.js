4646efdb2464dc2e887b3583bd8c490894415a97
Chisato Murakami <kiwipom1031@gmail.com>
2020-11-05 10:18:53 -0800

    fix styling in ChooseShippingOptions and SelectAddress

--- app/javascript/packs/components/ChooseShippingOptions.js
+++ app/javascript/packs/components/ChooseShippingOptions.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { Row, Col, ButtonGroup, ToggleButton } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  ButtonGroup,
+  ToggleButton,
+} from "react-bootstrap";
 import CheckOutInfo from "./CheckOutInfo";
 import SelectAddress from "./SelectAddress";
 
@@ -22,34 +28,36 @@ function ChooseShippingOptions() {
 
   return (
     <React.Fragment>
-      <Row className="ml-5">
-        <Col md={8}>
-          <h4>Choose your shipping options</h4>
-          <hr />
-          <Row className="mb-5 mt-5 pl-5">
-            <ButtonGroup toggle>
-              {radios.map((radio, idx) => (
-                <ToggleButton
-                  key={idx}
-                  type="radio"
-                  variant="secondary"
-                  name={radio.name}
-                  value={radio.value}
-                  checked={radioValue === radio.value}
-                  onChange={(e) => setRadioValue(e.currentTarget.value)}
-                  style={{ paddingLeft: 50, paddingRight: 50 }}
-                >
-                  {radio.name}
-                </ToggleButton>
-              ))}
-            </ButtonGroup>
-            {showSelectAddress()}
-          </Row>
-        </Col>
-        <Col md={4}>
-          <CheckOutInfo />
-        </Col>
-      </Row>
+      <Container>
+        <Row>
+          <Col md={9}>
+            <h4>Choose your shipping options</h4>
+            <hr />
+            <Row className="mb-5 mt-5 pl-5">
+              <ButtonGroup toggle>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    type="radio"
+                    variant="secondary"
+                    name={radio.name}
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                    style={{ paddingLeft: 50, paddingRight: 50 }}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+              {showSelectAddress()}
+            </Row>
+          </Col>
+          <Col md={3}>
+            <CheckOutInfo />
+          </Col>
+        </Row>
+      </Container>
     </React.Fragment>
   );
 }
--- app/javascript/packs/components/SelectAddress.js
+++ app/javascript/packs/components/SelectAddress.js
@@ -20,52 +20,49 @@ function SelectAddress(props) {
 
   return (
     <div>
-        <Row className="mt-5">
-          <Col md={8}>
-            <h4>Select a shipping address</h4>
-            <hr />
-            <Row className="mt-3">
-              <Col xs={12} sm={12} md={3} lg={3}>
-                <Link to={"/newaddress"}>
-                  <div className="address-add-btn">
-                    <p>Add Address</p>
-                  </div>
-                </Link>
-              </Col>
-              {props.addresses &&
-                props.addresses
-                  .sort((a, b) => (a.default < b.default ? 1 : -1))
-                  .map((address) => {
-                    return (
-                      <Col xs={12} sm={12} md={4} lg={4} key={address.id}>
-                        <div className="address">
-                          <p>
-                            {address.first_name} {address.last_name}
-                          </p>
-                          <p>
-                            <span>{address.street} </span>
-                            {address.apt_ste_unit && (
-                              <span>#{address.apt_ste_unit}</span>
-                            )}
-                          </p>
-                          <p>
-                            {address.city}, {address.state} {address.zip}
-                          </p>
-                          <p>
-                            <span>
-                              <SelectAddressBtn
-                                addressId={address.id}
-                              ></SelectAddressBtn>
-                            </span>
-                          </p>
-                        </div>
-                      </Col>
-                    );
-                  })}
-            </Row>
+      <Row className="mt-5">
+        <h4>Select a shipping address</h4>
+        <hr />
+        <Row className="mt-3">
+          <Col xs={12} sm={12} md={3} lg={3}>
+            <Link to={"/newaddress"}>
+              <div className="address-add-btn">
+                <p>Add Address</p>
+              </div>
+            </Link>
           </Col>
-          
+          {props.addresses &&
+            props.addresses
+              .sort((a, b) => (a.default < b.default ? 1 : -1))
+              .map((address) => {
+                return (
+                  <Col xs={12} sm={12} md={4} lg={4} key={address.id}>
+                    <div className="address">
+                      <p>
+                        {address.first_name} {address.last_name}
+                      </p>
+                      <p>
+                        <span>{address.street} </span>
+                        {address.apt_ste_unit && (
+                          <span>#{address.apt_ste_unit}</span>
+                        )}
+                      </p>
+                      <p>
+                        {address.city}, {address.state} {address.zip}
+                      </p>
+                      <p>
+                        <span>
+                          <SelectAddressBtn
+                            addressId={address.id}
+                          ></SelectAddressBtn>
+                        </span>
+                      </p>
+                    </div>
+                  </Col>
+                );
+              })}
         </Row>
+      </Row>
     </div>
   );
 }
