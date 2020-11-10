f81e810f2382e4aaba1d67f1f5eb122884611e93
Chisato Murakami <kiwipom1031@gmail.com>
2020-11-10 09:56:58 -0800

    adding UI in ReviewOrder

--- app/javascript/packs/components/ReviewOrder.js
+++ app/javascript/packs/components/ReviewOrder.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function ReviewOrder() {
   return (
-    <div>
-      Hello
-    </div>
-  )
+    <React.Fragment>
+      <Container>
+        <h4>Review your order</h4>
+        <hr />
+        <Row className="mb-5 mt-5">
+          <Col md={6}>
+            <h5>Shipping option</h5>
+            <div>.....</div>
+            <div>.....</div>
+          </Col>
+          <Col md={6}>
+            <h5>Payment method</h5>
+            <div>.....</div>
+            <div>.....</div>
+          </Col>
+        </Row>
+        <Row className="mb-5 mt-5"></Row>
+      </Container>
+    </React.Fragment>
+  );
 }
 
-export default ReviewOrder
+export default ReviewOrder;
