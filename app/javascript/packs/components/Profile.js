7538035f90f1ddb50a25fe679bbe781e0c12c844
Chisato Murakami <kiwipom1031@gmail.com>
2020-11-26 13:55:02 -0800

    add addresses component in profile

--- app/javascript/packs/components/Profile.js
+++ app/javascript/packs/components/Profile.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
+import Addresses from "./Addresses";
 
 export const Profile = () => {
   return (
-    <div>
-      profile.js
-    </div>
-  )
-}
+    <React.Fragment>
+      <div>Profile</div>
+      <div>
+        <Addresses />
+      </div>
+    </React.Fragment>
+  );
+};
 
-const mapStateToProps = (state) => ({
-  
-})
+const mapStateToProps = (state) => ({});
 
-
-
-export default connect(mapStateToProps)(Profile)
+export default connect(mapStateToProps)(Profile);
