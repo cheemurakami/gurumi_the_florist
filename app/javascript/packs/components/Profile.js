498add40507904873586bf30898bdd6f45a7be6c
Chisato Murakami <kiwipom1031@gmail.com>
2020-11-27 14:53:02 -0800

    add profile image

--- app/javascript/packs/components/Profile.js
+++ app/javascript/packs/components/Profile.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Container, Image } from "react-bootstrap";
 import Addresses from "./Addresses";
+import userImage from "./images/userImage.png";
 
 export const Profile = () => {
   return (
     <React.Fragment>
-      <div>Profile</div>
-      <div>
-        <Addresses />
-      </div>
+      <Container>
+        <Image className="ml-3" src={userImage} roundedCircle  width={150}
+            height={150}/>
+        <div>
+          <Addresses />
+        </div>
+      </Container>
     </React.Fragment>
   );
 };
