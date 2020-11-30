fa90e9c1ad7f9d34049c6378a583798464e4ce2f
Chisato Murakami <kiwipom1031@gmail.com>
2020-11-29 17:46:29 -0800

    connect redux to show currentUser in Profile.js

--- app/javascript/packs/components/Profile.js
+++ app/javascript/packs/components/Profile.js
@@ -4,20 +4,36 @@ import { Container, Image } from "react-bootstrap";
 import Addresses from "./Addresses";
 import userImage from "./images/userImage.png";
 
-export const Profile = () => {
+export const Profile = (props) => {
+  const { currentUser } = props;
+  const showUser = () => {
+    if(currentUser){
+      return (
+        <h4>{currentUser.email}</h4>
+      )
+    }
+  }
   return (
     <React.Fragment>
       <Container>
-        <Image className="ml-3" src={userImage} roundedCircle  width={150}
-            height={150}/>
-        <div>
-          <Addresses />
-        </div>
+        <Image
+          className="ml-3"
+          src={userImage}
+          roundedCircle
+          width={150}
+          height={150}
+        />
+        {showUser()}
+        <Addresses />
       </Container>
     </React.Fragment>
   );
 };
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.loginStatusReducer.currentUser,
+  };
+};
 
 export default connect(mapStateToProps)(Profile);
