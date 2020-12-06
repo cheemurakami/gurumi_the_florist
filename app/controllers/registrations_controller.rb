e2d3da236ac610effd994ede72b2176cfac9156a
Chisato Murakami <kiwipom1031@gmail.com>
2020-12-05 21:52:38 -0800

    add configure_sanitized_params action in registrations_controller so it updates username in database

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
  respond_to :json
- 
+ before_action :configure_sanitized_params, if: :devise_controller?
+
+  protected
+  def configure_sanitized_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
 
-# ここでjsonにしてる
-# -> compに帰る
+# jsonにさせるために作ったファイル
+# configure_sanitized_paramsはaction
