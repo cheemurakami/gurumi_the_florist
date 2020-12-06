2d5f32f02375d0b8633f0afe59d19adb014fd312
Chisato Murakami <kiwipom1031@gmail.com>
2020-12-06 13:14:37 -0800

    add migration for first_name and last_name in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_001800) do
+ActiveRecord::Schema.define(version: 2020_12_06_211335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,6 +129,8 @@ ActiveRecord::Schema.define(version: 2020_11_29_001800) do
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
     t.string "username"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
