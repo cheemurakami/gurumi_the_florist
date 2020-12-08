a54056b408819154dcd925604dd6f9082756002f
Chisato Murakami <kiwipom1031@gmail.com>
2020-12-07 19:55:04 -0800

    add keys in permission in registrations_controller.rb

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   protected
   def configure_sanitized_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name])
   end
 end
 
